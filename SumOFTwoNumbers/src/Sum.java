548e8874e62b831a97bea0b669f1def5e202a408
atulshekhawat21 <atulshekhawat21@gmail.com>
2023-10-26 20:02:58 +0530

    Day1

---
+++ SumOFTwoNumbers/src/Sum.java
@@ -0,0 +1,14 @@
+public class Sum {
+
+    public static void Sum(int a , int b){
+        int c =a+b;
+        System.out.println("The Sum of Two Number is " +c);
+    }
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        Sum(23,5);
+
+    }
+}
---
+++ areaOfCircle/src/areaOfCircle.java
@@ -0,0 +1,12 @@
+
+public class areaOfCircle {
+    public static void areaOfCircle(double r){
+        double areaOfCircle = Math.PI *r*r;
+        System.out.println("Area Of Circle is "+Math.round(areaOfCircle));
+    }
+    public static void main(String[] args) {
+
+        int r;
+        areaOfCircle(5);
+    }
+}
