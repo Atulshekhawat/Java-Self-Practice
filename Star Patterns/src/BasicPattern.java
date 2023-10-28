037bce2cc8505e6debe3447c40666c318fccb746
atulshekhawat21 <atulshekhawat21@gmail.com>
2023-10-28 21:48:54 +0530

    Adding some more pattern problems

--- Star Patterns/src/BasicPattern.java
+++ Star Patterns/src/BasicPattern.java
@@ -2,7 +2,7 @@ public class BasicPattern {
 
     public static  void solidRectangle(){
         System.out.println("Rectangular Pattern");
-        for (int i=0;i<=3;i++){
+        for (int i=1;i<=3;i++){
             for (int j=1;j<=7;j++){
                 System.out.print("* ");
             }
@@ -22,8 +22,61 @@ public class BasicPattern {
         }
     }
 
+    public static void halfPyramidWithNumber(){
+        System.out.println();
+        int n=5;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(j+"");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void reverseHalfPyramid(){
+        System.out.println("Inverted Half Pyramid");
+        int n=5;
+         for (int i=n;i>=1;i--){
+             for (int j=1;j<i;j++){
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+    }
+
+    public static  void invertedHalfPyramidWithNumber(){
+        System.out.println("Inverted Half Pyramid With Number");
+        int n=5;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void reverseHalfPyramid180(){
+        System.out.println("Reverse Half Pyramid 180 rotated");
+        int n=5;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++ ){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+
     public static void main(String[] args) {
-        solidRectangle();
-        halfPyramid();
+//        solidRectangle();
+//        halfPyramid();
+//        halfPyramidWithNumber();
+//        reverseHalfPyramid();
+//        reverseHalfPyramid180();
+        invertedHalfPyramidWithNumber();
     }
 }
